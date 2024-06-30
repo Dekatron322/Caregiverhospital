d097240bfbfe2ae1b3f49c904c5b729449afe931
Dekatron322 <muritalaibrahim097@gmail.com>
2024-06-30 23:03:44 +0100

    The MVP

--- components/Patient/CustomDropdown.tsx
+++ components/Patient/CustomDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { MdKeyboardArrowDown } from "react-icons/md"
 import styles from "./CustomDropdown.module.css"
 import Image from "next/image"
